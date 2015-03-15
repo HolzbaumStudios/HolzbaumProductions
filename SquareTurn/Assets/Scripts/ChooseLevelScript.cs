80116e751e33d82896ed46cf679313f15bedb3d1
fbHolzbaum <blancofel22@gmail.com>
2015-03-15 11:59:47 +0100

    Added lost Statistics
    -

--- SquareTurn/Assets/Scripts/ChooseLevelScript.cs
+++ SquareTurn/Assets/Scripts/ChooseLevelScript.cs
@@ -5,6 +5,12 @@ public class ChooseLevelScript : MonoBehaviour {
 	
 
 	public void LoadLevel(int level){
+		//Update Statistics --> Played Levels
+		int startedLevels = PlayerPrefs.GetInt ("NumberOfStartedLevels");
+		startedLevels++;
+		PlayerPrefs.SetInt ("NumberOfStartedLevels", startedLevels);
+		
+		//Start the Level
 		PlayerPrefs.SetInt ("ChosenLevel", level);
 		Application.LoadLevel ("gameScene");
 	}
--- SquareTurn/Assets/Scripts/UserStatistics.cs
+++ SquareTurn/Assets/Scripts/UserStatistics.cs
@@ -14,6 +14,7 @@ public class UserStatistics : MonoBehaviour {
 
 	int totalNumberOfMoves;  //How many moves the user has made in the whole game
 	int totalNumberOfTurns; //How many squares the user has turned in the whole game
+	int totalNumberOfResets;
 
 	// ---------------------------INITIALIZATION----------------------------------------------
 	
@@ -21,7 +22,7 @@ public class UserStatistics : MonoBehaviour {
 		//Store the prefabs in the local variables
 		totalNumberOfMoves = PlayerPrefs.GetInt ("TotalNumberOfMoves");
 		totalNumberOfTurns = PlayerPrefs.GetInt ("TotalNumberOfTurns");
-
+		totalNumberOfResets = PlayerPrefs.GetInt ("TotalNumberOfResets");
 	}
 
 	// --------------------------FUNCTIONS---------------------------------------------------
@@ -36,9 +37,11 @@ public class UserStatistics : MonoBehaviour {
 			break;
 			case "Turns++": totalNumberOfTurns = totalNumberOfTurns + amount;
 			break;
+			case "Resets++": totalNumberOfResets += amount;
+			break;
 		}
 
-		Debug.Log ("Moves: " + totalNumberOfMoves + " Turns: " + totalNumberOfTurns);
+		Debug.Log ("Moves: " + totalNumberOfMoves + " Turns: " + totalNumberOfTurns + " Resets: " + totalNumberOfResets);
 	}
 
 
@@ -48,6 +51,7 @@ public class UserStatistics : MonoBehaviour {
 	public void StoreStatistics(){
 		PlayerPrefs.SetInt ("TotalNumberOfMoves", totalNumberOfMoves);
 		PlayerPrefs.SetInt ("TotalNumberOfTurns", totalNumberOfTurns);
+		PlayerPrefs.SetInt ("TotalNumberOfResets", totalNumberOfResets);
 	}
 
 	
