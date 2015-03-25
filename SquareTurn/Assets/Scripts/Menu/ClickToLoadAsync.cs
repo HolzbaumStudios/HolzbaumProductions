a202a914c37eb9cc0e7c58a4d20428cbf23afd2b
SirWoody <roman.martinello@gmail.com>
2015-03-25 19:33:03 +0100

    startMenu
    Startmenu hinzugefügt inkl. Musik und Scripts.

---
+++ SquareTurn/Assets/Scripts/Menu/ClickToLoadAsync.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClickToLoadAsync : MonoBehaviour {
+
+	public Slider Slider_Loading;
+	public GameObject Image_Loading;
+
+	private AsyncOperation async;
+
+	public void ClickAsync(int level)
+	{
+		Image_Loading.SetActive(true);
+		StartCoroutine(LoadLevelWithBar(level));
+	}
+
+	IEnumerator LoadLevelWithBar (int level)
+	{
+		async = Application.LoadLevelAsync (level);
+		while (!async.isDone) 
+		{
+			Slider_Loading.value = async.progress;
+			yield return null;
+		}
+	}
+
+}
---
+++ SquareTurn/Assets/Scripts/Menu/LoadOnClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+	public void LoadScene(int scene)
+	{
+		Application.LoadLevel (scene);
+	}
+
+}
