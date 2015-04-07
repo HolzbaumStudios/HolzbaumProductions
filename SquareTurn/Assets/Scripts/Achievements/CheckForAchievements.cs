dd1f62cc66c355915bdb116de43c8d1d4290a418
fbHolzbaum <blancofel22@gmail.com>
2015-04-07 22:11:19 +0200

    Continued Achievement Box
    -Added Facebook Share Functionality
    -Added Animation

---
+++ SquareTurn/Assets/Scripts/Achievements/CheckForAchievements.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckForAchievements : MonoBehaviour {
+
+	public GameObject achievementPanel;
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefs.SetInt ("NewAchievement", 1);
+
+		if (PlayerPrefs.GetInt ("NewAchievement") == 1) {
+			achievementPanel.SetActive (true);
+			achievementPanel.GetComponent<Animation>().Play ();
+		}
+	}
+
+}
---
+++ SquareTurn/Assets/Scripts/Achievements/ShareOnFacebook.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Facebook;
+using Facebook.MiniJSON;
+
+public class ShareOnFacebook : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake(){
+		FB.Init(SetInit, OnHideUnity);
+	}
+
+	public void ShareAchievement(){
+
+		if (!FB.IsLoggedIn) {
+			LoginToFacebook ();
+		}
+
+		FB.Feed(                                                                                                                 
+	        linkCaption: "I just unlocked the Banana Achievement! Can you do it too?",               
+	        picture: "http://www.friendsmash.com/images/logo_large.jpg",                                                     
+	        linkName: "Checkout Squared!",                                                                 
+	        link: "http://apps.facebook.com/" + FB.AppId + "/?challenge_brag=" + (FB.IsLoggedIn ? FB.UserId : "guest")       
+        ); 
+	}
+	
+	private void LoginToFacebook(){
+		FB.Login("email,publish_actions", LoginCallback);
+	}
+
+
+
+
+
+
+
+	/// <summary>
+	/// Facebook sdk relevant functions
+	/// </summary>
+	
+	private void SetInit() {
+		enabled = true; 
+		// "enabled" is a magic global; this lets us wait for FB before we start rendering
+	}
+	
+	private void OnHideUnity(bool isGameShown) {
+		if (!isGameShown) {
+			// pause the game - we will need to hide
+			Time.timeScale = 0;
+		} else {
+			// start the game back up - we're getting focus again
+			Time.timeScale = 1;
+		}
+	}
+
+	void LoginCallback(FBResult result)                                                        
+	{                                                                                          
+		Util.Log("LoginCallback");                                                          
+		
+		if (FB.IsLoggedIn)                                                                     
+		{                                                                                      
+			OnLoggedIn();                                                                      
+		}
+	} 
+
+	void OnLoggedIn()                                                                          
+	{                                                                                          
+		Util.Log("Logged in. ID: " + FB.UserId);                                            
+	}  
+	
+	
+}
