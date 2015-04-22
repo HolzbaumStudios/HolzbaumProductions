2ba08d71c0a652590f08cf6c6b6b33178df704f2
SirWoody <roman.martinello@gmail.com>
2015-04-22 22:30:13 +0200

    Menu includes Credits

---
+++ SquareTurn/Assets/Scripts/Menu/turnExitOnClick.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turnExitOnClick : MonoBehaviour {
+	
+	// Variablen
+	public GameObject squareE;
+	public GameObject squareX;
+	public GameObject squareI;
+	public GameObject squareT;
+	
+	// Funktionen
+	public void TurnButton()
+	{
+		StartCoroutine (TurnExit ());
+	}
+	
+	
+	
+	
+	//Coroutine
+	IEnumerator TurnExit() {
+		squareE.GetComponent<Animation> ().Play();
+		yield return new WaitForSeconds(0.25f);
+		squareE.GetComponent<UnityEngine.UI.Image> ().color = new Color32(240, 120, 48, 255);
+		squareE.transform.FindChild ("Letter").gameObject.SetActive (false);
+		squareX.GetComponent<Animation> ().Play();
+		yield return new WaitForSeconds(0.25f);
+		squareX.GetComponent<UnityEngine.UI.Image> ().color = new Color32(240, 120, 48, 255);
+		squareX.transform.FindChild ("Letter").gameObject.SetActive (false);
+		squareI.GetComponent<Animation> ().Play();
+		yield return new WaitForSeconds(0.25f);
+		squareI.GetComponent<UnityEngine.UI.Image> ().color = new Color32(240, 120, 48, 255);
+		squareI.transform.FindChild ("Letter").gameObject.SetActive (false);
+		squareT.GetComponent<Animation> ().Play();
+		yield return new WaitForSeconds(0.25f);
+		squareT.GetComponent<UnityEngine.UI.Image> ().color = new Color32(240, 120, 48, 255);
+		squareT.transform.FindChild ("Letter").gameObject.SetActive (false);
+		yield return new WaitForSeconds(0.5f);
+		Application.Quit();
+	}
+	
+	
+}
