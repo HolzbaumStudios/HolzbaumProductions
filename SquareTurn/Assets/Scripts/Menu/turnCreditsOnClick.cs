6876f64b2c298a002e3ba2b1b16a98a0cd89be27
SirWoody <roman.martinello@gmail.com>
2015-05-20 20:19:36 +0200

    credit button function

---
+++ SquareTurn/Assets/Scripts/Menu/turnCreditsOnClick.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turnCreditsOnClick : MonoBehaviour {
+
+	// Variablen
+	public GameObject squareC;
+	public GameObject squareR;
+	public GameObject squareE;
+	public GameObject squareD;
+	public GameObject squareI;
+	public GameObject squareT;
+	public GameObject squareS;
+
+	// Funktionen
+	public void TurnButton()
+	{
+		StartCoroutine(TurnPlay ());
+	}
+
+	//Coroutine
+	IEnumerator TurnPlay() {
+		squareC.GetComponent<Animation> ().Play();
+		yield return new WaitForSeconds(0.125f);
+		squareC.GetComponent<UnityEngine.UI.Image> ().color = new Color32(240, 120, 48, 255);
+		squareC.transform.FindChild ("Letter").gameObject.SetActive (false);
+		squareR.GetComponent<Animation> ().Play();
+		yield return new WaitForSeconds(0.125f);
+		squareR.GetComponent<UnityEngine.UI.Image> ().color = new Color32(240, 120, 48, 255);
+		squareR.transform.FindChild ("Letter").gameObject.SetActive (false);
+		squareE.GetComponent<Animation> ().Play();
+		yield return new WaitForSeconds(0.125f);
+		squareE.GetComponent<UnityEngine.UI.Image> ().color = new Color32(240, 120, 48, 255);
+		squareE.transform.FindChild ("Letter").gameObject.SetActive (false);
+		squareD.GetComponent<Animation> ().Play();
+		yield return new WaitForSeconds(0.125f);
+		squareD.GetComponent<UnityEngine.UI.Image> ().color = new Color32(240, 120, 48, 255);
+		squareD.transform.FindChild ("Letter").gameObject.SetActive (false);
+		squareI.GetComponent<Animation> ().Play();
+		yield return new WaitForSeconds(0.125f);
+		squareI.GetComponent<UnityEngine.UI.Image> ().color = new Color32(240, 120, 48, 255);
+		squareI.transform.FindChild ("Letter").gameObject.SetActive (false);
+		squareT.GetComponent<Animation> ().Play();
+		yield return new WaitForSeconds(0.125f);
+		squareT.GetComponent<UnityEngine.UI.Image> ().color = new Color32(240, 120, 48, 255);
+		squareT.transform.FindChild ("Letter").gameObject.SetActive (false);
+		squareS.GetComponent<Animation> ().Play();
+		yield return new WaitForSeconds(0.125f);
+		squareS.GetComponent<UnityEngine.UI.Image> ().color = new Color32(240, 120, 48, 255);
+		squareS.transform.FindChild ("Letter").gameObject.SetActive (false);
+		yield return new WaitForSeconds(0.25f);
+		Application.LoadLevel ("temp_menu");
+	}
+}
