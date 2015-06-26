15cdaa912192d5fc25e4b2c5892e43d116250148
fbHolzbaum <hhfblanc@CHHHMPC1000.eu.hcnet.biz>
2015-06-26 12:43:53 +0200

    Progress Circle Functionality

---
+++ SquareTurn/Assets/Scripts/Achievements/AchievementsProgressScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AchievementsProgressScript : MonoBehaviour {
+
+	[Range(1,30)]
+	public int numberOfAchievements;
+
+	public GameObject progressCircle;
+	public GameObject progressPercent;
+
+
+	private int unlockedAchievements;
+	private int completedPercent;
+	
+	// Update is called once per frame
+	void Start () {
+
+
+		unlockedAchievements = PlayerPrefs.GetInt ("UnlockedAchievements");
+		unlockedAchievements = 4;
+		completedPercent = 100 / numberOfAchievements * unlockedAchievements;
+
+		progressCircle.GetComponent<Image>().fillAmount = completedPercent/100f;
+		progressPercent.GetComponent<Text>().text = completedPercent.ToString ();
+	
+	}
+}
