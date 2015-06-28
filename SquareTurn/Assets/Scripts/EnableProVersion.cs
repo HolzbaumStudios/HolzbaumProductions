86b832431153eeec567d90ead6a0728b7a7e5880
fbHolzbaum <blancofel22@gmail.com>
2015-06-28 22:33:35 +0200

    Added Pro Functionality
    -Script with bool to set pro/free version
    -Star Info Box in the game Scene

---
+++ SquareTurn/Assets/Scripts/EnableProVersion.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableProVersion : MonoBehaviour {
+
+	public bool proVersion;
+
+}
---
+++ SquareTurn/Assets/Scripts/GameLogic/SetStarInfo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetStarInfo : MonoBehaviour {
+
+	public GameObject starInfoBoxLandscape;
+	public GameObject starInfoBoxPortrait;
+
+	public GameObject threeStarInfoLandscape;
+	public GameObject twoStarInfoLandscape;
+
+	public GameObject threeStarInfoPortrait;
+	public GameObject twoStarInfoPortrait;
+
+	// Use this for initialization
+	void Start () {
+
+		//Check if Pro Mode is enabled
+		if(GameObject.Find ("UserStatistics").GetComponent<EnableProVersion>().proVersion == true)
+		{
+			starInfoBoxLandscape.SetActive(true);
+			starInfoBoxPortrait.SetActive(true);
+		}
+	
+	}
+	
+}
