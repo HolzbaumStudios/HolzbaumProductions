2804901876c15a98c36b4b64b4a09ca83d01d780
fbHolzbaum <blancofel22@gmail.com>
2015-07-31 22:59:39 +0200

    Added Achievement Descriptions and images

--- SquareTurn/Assets/Scripts/Achievements/AchievementCollection.cs
+++ SquareTurn/Assets/Scripts/Achievements/AchievementCollection.cs
@@ -46,11 +46,22 @@ public class AchievementCollection : MonoBehaviour {
 
 	/////////////////////VARIABLES//////////////////////////////
 	//Pictures
-	
+	public Sprite spriteAchievement0;
 	public Sprite spriteAchievement1;
 	public Sprite spriteAchievement2;
 	public Sprite spriteAchievement3;
 	public Sprite spriteAchievement4;
+	public Sprite spriteAchievement5;
+	public Sprite spriteAchievement6;
+	public Sprite spriteAchievement7;
+	public Sprite spriteAchievement8;
+	public Sprite spriteAchievement9;
+	public Sprite spriteAchievement10;
+	public Sprite spriteAchievement11;
+	public Sprite spriteAchievement12;
+	public Sprite spriteAchievement13;
+	public Sprite spriteAchievement14;
+	public Sprite spriteAchievement15;
 
 	//Create Lists
 	List<Sprite> achievementSpriteList = new List<Sprite> ();
@@ -58,8 +69,25 @@ public class AchievementCollection : MonoBehaviour {
 
 	//Create Class Variables
 	//classvariables have to be added to the list in the awake function
-	Achievement achievement1 = new Achievement("Plant the seeds!", "Unlock 10 trees.");  //list number 0
-	Achievement achievement2 = new Achievement("Addicted to squares!", "Start the game 50 times."); //list number 1
+	Achievement achievement0 = new Achievement("Planting the seed", "Solve every level from pack 1");
+	Achievement achievement1 = new Achievement("Growing stem", "Get all the stars in pack 1");
+	Achievement achievement2 = new Achievement("Time to blossom", "Solve every level from pack 2"); 
+	Achievement achievement3 = new Achievement("Climbing tree", "Get all the stars in pack 2"); 
+	Achievement achievement4 = new Achievement("Flowerfield", "Solve every level from pack 3"); 
+	Achievement achievement5 = new Achievement("Rainforest dryer", "Get all the stars in pack 3"); 
+
+	Achievement achievement6 = new Achievement("First steps", "Solve 1 level");
+	Achievement achievement7 = new Achievement("Amateur", "Solve 10 levels"); 
+	Achievement achievement8 = new Achievement("Challenger", "Solve 30 levels"); 
+	Achievement achievement9 = new Achievement("Blackbelt", "Solve 75 levels");
+
+	Achievement achievement10 = new Achievement("Turnaround", "Turn 200 tiles"); 
+	Achievement achievement11 = new Achievement("Turning tables", "Turn 1000 tiles"); 
+	Achievement achievement12 = new Achievement("Flippin' Ninja", "Turn 5000 tiles");
+
+	Achievement achievement13 = new Achievement("Child", "Start the game 10 times"); 
+	Achievement achievement14 = new Achievement("Adult", "Start the game 50 times"); 
+	Achievement achievement15 = new Achievement("Veteran", "Start the game 100 times"); 
 
 
 	
@@ -71,24 +99,52 @@ public class AchievementCollection : MonoBehaviour {
 	/// 
 	void Awake(){
 		//Initialize the class List
+		achievementList.Add(achievement0);
 		achievementList.Add(achievement1);
 		achievementList.Add(achievement2);
+		achievementList.Add(achievement3);
+		achievementList.Add(achievement4);
+		achievementList.Add(achievement5);
+		achievementList.Add(achievement6);
+		achievementList.Add(achievement7);
+		achievementList.Add(achievement8);
+		achievementList.Add(achievement9);
+		achievementList.Add(achievement10);
+		achievementList.Add(achievement11);
+		achievementList.Add(achievement12);
+		achievementList.Add(achievement13);
+		achievementList.Add(achievement14);
+		achievementList.Add(achievement15);
 
 		//Initialize the sprite list
+		achievementSpriteList.Add (spriteAchievement0);
 		achievementSpriteList.Add (spriteAchievement1);
 		achievementSpriteList.Add (spriteAchievement2);
+		achievementSpriteList.Add (spriteAchievement3);
+		achievementSpriteList.Add (spriteAchievement4);
+		achievementSpriteList.Add (spriteAchievement5);
+		achievementSpriteList.Add (spriteAchievement6);
+		achievementSpriteList.Add (spriteAchievement7);
+		achievementSpriteList.Add (spriteAchievement8);
+		achievementSpriteList.Add (spriteAchievement9);
+		achievementSpriteList.Add (spriteAchievement10);
+		achievementSpriteList.Add (spriteAchievement11);
+		achievementSpriteList.Add (spriteAchievement12);
+		achievementSpriteList.Add (spriteAchievement13);
+		achievementSpriteList.Add (spriteAchievement14);
+		achievementSpriteList.Add (spriteAchievement15);
+
 
 
 		//Get the state for all achievements at the beginning
 		for (int i = 0; i < achievementList.Count; i++)
 		{
 			string prefabName = "Achievement" + i + "State";
-			Debug.Log (prefabName);
 			achievementList[i].SetState (PlayerPrefs.GetInt (prefabName));
 		}
 
 
-		achievementList [0].SetState (2); //--> just for testing purposes
+		//achievementList [0].SetState (1); //--> just for testing purposes
 		//achievementList [1].SetState (1); //--> just for testing purposes
 	}
 
@@ -102,13 +158,15 @@ public class AchievementCollection : MonoBehaviour {
 		if(achievementNumber != 100)
 		{ 
 			achievementPanel.SetActive (true);
-			achievementPanel.GetComponent<Animation>().Play ();
+			//achievementPanel.GetComponent<Animation>().Play ();
 
 			GameObject achievementImage = achievementPanel.transform.FindChild ("AchievementImage").gameObject;
 			GameObject achievementText = achievementPanel.transform.FindChild ("AchievementText").gameObject;
 
 			achievementImage.GetComponent<Image>().sprite = achievementSpriteList[achievementNumber];
 			achievementText.GetComponent<Text>().text = achievementList[achievementNumber].GetTitle();
+
+			StartCoroutine(AnimationCountdown(6));
 		}
 		else
 		{
@@ -135,7 +193,7 @@ public class AchievementCollection : MonoBehaviour {
 	///Close achievement panel
 	//If the player clicks ok, this function is run
 	public void CloseAchievementPanel(){
-		GameObject achievementPanel = GameObject.Find ("AchievementPanel");
+		GameObject achievementPanel = GameObject.Find ("AchievementPanelBase");
 		achievementPanel.SetActive (false);
 		SetAchievementWindow(achievementPanel); //Call the function again, to determine if there are more unlocked achievements
 	}
@@ -158,6 +216,13 @@ public class AchievementCollection : MonoBehaviour {
 			}
 		}
 
+	}
+
+	//Wait a specified time of seconds, until disabling the animation panel
+	IEnumerator AnimationCountdown(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		CloseAchievementPanel ();
 	}
 
 }
