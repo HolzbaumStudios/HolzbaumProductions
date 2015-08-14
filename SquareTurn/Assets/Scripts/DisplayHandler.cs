809bb1297d20b2b9c473bf44d14d8fb8336b3789
fbHolzbaum <hhfblanc@CHHHMPC1000.eu.hcnet.biz>
2015-08-14 19:11:25 +0200

    Started Device Position Handler

---
+++ SquareTurn/Assets/Scripts/DisplayHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		GameObject portraitCanvas = GameObject.Find ("StartMenuCanvasPortrait");
+		GameObject landscapeCanvas = GameObject.Find ("StartMenuCanvas");
+
+		if(Screen.width < Screen.height)
+		{
+			portraitCanvas.SetActive(true);
+			landscapeCanvas.SetActive(false);
+		}else{
+			portraitCanvas.SetActive(false);
+			landscapeCanvas.SetActive(true);
+		}
+	}
+
+}
