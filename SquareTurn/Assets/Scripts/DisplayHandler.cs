1ce55e977fa01dd69abbf8a45854df4337fe0378
fbHolzbaum <blancofel22@gmail.com>
2015-08-16 15:58:20 +0200

    Enabled AutoRotation for LevelChoice

--- SquareTurn/Assets/Scripts/DisplayHandler.cs
+++ SquareTurn/Assets/Scripts/DisplayHandler.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class DisplayHandler : MonoBehaviour {
 
+
+	private GameObject portraitCanvas;
+	private GameObject landscapeCanvas;
+
 	// Use this for initialization
 	void Awake () {
-		GameObject portraitCanvas = GameObject.Find ("StartMenuCanvasPortrait");
-		GameObject landscapeCanvas = GameObject.Find ("StartMenuCanvas");
+		portraitCanvas = GameObject.Find ("CanvasPortrait");
+		landscapeCanvas = GameObject.Find ("CanvasLandscape");
+	}
 
+	void Update()
+	{
 		if(Screen.width < Screen.height)
 		{
 			portraitCanvas.SetActive(true);
