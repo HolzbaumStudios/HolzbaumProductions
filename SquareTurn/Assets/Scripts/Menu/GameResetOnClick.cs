d6a24223f40e811a5635c04758a494a08a196678
SirWoody <roman.martinello@gmail.com>
2015-12-16 18:08:33 +0100

    Woody Update
    Added Reset Button with script.
    Adjusted the credits
    resovled bug in statistics scene on portrait where a bit of text got cut
    off
    worked on the scaling for the buttons in credits scene

---
+++ SquareTurn/Assets/Scripts/Menu/GameResetOnClick.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameResetOnClick : MonoBehaviour {
+
+	//Reset Function
+    public void ResetGame()
+    {
+        // Reset Game Stats
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.SetInt("NumberOfAppStarts", 0);
+        //PlayerPrefs.SetInt("TotalNumberOfMoves", 0);
+        //PlayerPrefs.SetInt("TotalNumberOfTurn", 0);
+        //PlayerPrefs.SetInt("TotalNumberOfResets", 0);
+        // Es fehlt ein Reset für alle Sterne (klappt mit Delete All)
+
+    } 
+}
