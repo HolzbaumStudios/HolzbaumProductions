79e34f4f3c5f2e297c5504dd63ccaa11497bd2e2
fbHolzbaum <blancofel22@gmail.com>
2016-02-01 23:47:31 +0100

    Continued working on script

--- SquareTurn/Assets/Scripts/DisplayHandler/CreateLevelMenuLayout.cs
+++ SquareTurn/Assets/Scripts/DisplayHandler/CreateLevelMenuLayout.cs
@@ -28,6 +28,7 @@ public class CreateLevelMenuLayout : MonoBehaviour {
         //-----Create LevelChoice Panel-----
         GameObject levelChoice = new GameObject("LevelChoice");
         levelChoice.transform.SetParent(this.gameObject.transform);
+        levelChoice.layer = LayerMask.NameToLayer("UI");
         levelChoice.AddComponent<CanvasRenderer>();
         levelChoice.AddComponent<RectTransform>();
         //Set rect transform size
@@ -50,12 +51,13 @@ public class CreateLevelMenuLayout : MonoBehaviour {
     }
 
     //Create a category
-    //This function takes the category number and the parent to which the categories have to be attached as argument
+    //This function takes the category number and the parent to which the categories have to be attached, as argument
     void CreateCategory(int cat, Transform parent)
     {
         //Create Category Object
         GameObject category = new GameObject("Category" + cat);
         category.transform.SetParent(parent);
+        category.layer = LayerMask.NameToLayer("UI");
         category.AddComponent<CanvasRenderer>();
         category.AddComponent<RectTransform>();
         //Set rect transform size
@@ -68,6 +70,64 @@ public class CreateLevelMenuLayout : MonoBehaviour {
         categoryRect.anchorMax = new Vector2(1, 1);
         categoryRect.pivot = new Vector2(0.5f, 0.5f);
 
+        //Create LevelContainers (12 levels each)
+        GameObject container1 = new GameObject("Level" + cat + "00-" + cat +"11");
+        GameObject container2 = new GameObject("Level" + cat + "12-" + cat + "23");
+        container1.layer = LayerMask.NameToLayer("UI");
+        container2.layer = LayerMask.NameToLayer("UI");
+        container1.transform.SetParent(category.transform);
+        container2.transform.SetParent(category.transform);
+        container1.AddComponent<CanvasRenderer>();
+        container1.AddComponent<RectTransform>();
+        container2.AddComponent<CanvasRenderer>();
+        container2.AddComponent<RectTransform>();
+        //Set rect transform anchors
+        RectTransform container1Rect = container1.GetComponent<RectTransform>();
+        RectTransform container2Rect = container2.GetComponent<RectTransform>();
+        container1Rect.anchorMin = new Vector2(0, 0.5f);
+        container1Rect.anchorMax = new Vector2(0, 0.5f);
+        container1Rect.pivot = new Vector2(0.5f, 0.5f);
+        container2Rect.anchorMin = new Vector2(0, 0.5f);
+        container2Rect.anchorMax = new Vector2(0, 0.5f);
+        container2Rect.pivot = new Vector2(0.5f, 0.5f);
+        //Set rect transform size
+        container1Rect.anchoredPosition = new Vector2(resolutionWidth*0.5f, 0);
+        container1Rect.sizeDelta = new Vector2(resolutionWidth, resolutionHeight);
+        container1Rect.localScale = new Vector3(1, 1, 1);
+        container2Rect.anchoredPosition = new Vector2(resolutionWidth*1.5f, 0);
+        container2Rect.sizeDelta = new Vector2(resolutionWidth, resolutionHeight);
+        container2Rect.localScale = new Vector3(1, 1, 1);
+
+        //-----Create levelbuttons-----
+        for(int i = 0; i<24; i++)
+        {
+            if(i < 12) //Attach level 0 - 11 to container1 and 12 - 23 to container2
+            {
+                CreateLevelButton(i, cat, container1.transform);
+            }
+            else
+            {
+                CreateLevelButton(i, cat, container2.transform);
+            }
+        }
+    }
+
+    //Create a levelbutton
+    //This function takes the level number and the parent to which the categories have to be attached, as argument
+    void CreateLevelButton(int buttonNumber, int category, Transform parent)
+    {
+        string levelNumber;
+        if(buttonNumber < 10)
+        {
+            levelNumber = category + "0" + buttonNumber;
+        }
+        else
+        {
+            levelNumber = category + "" + buttonNumber;
+        }
+        GameObject levelButton = new GameObject("Level" + levelNumber + "ButtonPanel");
+        levelButton.transform.SetParent(parent);
+        levelButton.layer = LayerMask.NameToLayer("UI");
     }
 
 
