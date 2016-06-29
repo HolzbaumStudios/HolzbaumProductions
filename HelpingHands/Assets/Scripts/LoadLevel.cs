af68daa6a3972d09e3809804ade5d29779c063fb
SirWoody <roman.martinello@gmail.com>
2016-06-29 14:49:33 +0200

    Added first 3 layers of the project
    Titelbild
    Themenauswahl
    Themen in Themenauswahl

---
+++ HelpingHands/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+    public string LevelToLoad;
+
+
+    // Use this for initialization
+    public void loadLevel()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
