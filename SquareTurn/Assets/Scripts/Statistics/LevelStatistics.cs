bb7d64d92aabf1c7f5811c9486e601aa6f376646
fbHolzbaum <blancofel22@gmail.com>
2018-12-02 12:05:51 +0100

    Changed the main menu

---
+++ SquareTurn/Assets/Scripts/Statistics/LevelStatistics.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStatistics : MonoBehaviour {
+
+    public static string STARS_PER_LEVEL_PREFAB = "StarsLevel";
+    public static string COMPLETED_LEVELS_PER_CAT = "CompletedLevelsCat";
+
+    int[] starsCategoryOne = new int[24];
+    int[] starsCategoryTwo = new int[24];
+    int[] starsCategoryThree = new int[24];
+    int[] starsCategoryFour = new int[24];
+
+    int[] completedLevelsPerCategory = new int[4];
+
+    private static LevelStatistics instance;
+
+    public void Awake()
+    {
+        if(instance ==  null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        SetCategoryStarsFromPrefab(ref starsCategoryOne, 100);
+        SetCategoryStarsFromPrefab(ref starsCategoryTwo, 200);
+        SetCategoryStarsFromPrefab(ref starsCategoryThree, 300);
+        SetCategoryStarsFromPrefab(ref starsCategoryFour, 400);
+
+        for(int i = 0; i < completedLevelsPerCategory.Length; i++)
+        {
+            completedLevelsPerCategory[i] = PlayerPrefs.GetInt(COMPLETED_LEVELS_PER_CAT + i);
+        }
+    }
+
+    public void SetCategoryStarsFromPrefab(ref int[] categoryArray, int categoryNumber)
+    {
+        for (int i = 0; i < categoryArray.Length; i++)
+        {
+            categoryArray[i] = PlayerPrefs.GetInt(STARS_PER_LEVEL_PREFAB + (categoryNumber + 0));
+        }
+    }
+
+    public static LevelStatistics GetInstance()
+    {
+        return instance;
+    }
+}
--- SquareTurn/Assets/Scripts/Statistics/PrepareStatisticScene.cs
+++ SquareTurn/Assets/Scripts/Statistics/PrepareStatisticScene.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using TMPro;
 
 public class PrepareStatisticScene : MonoBehaviour {
 
-	public GameObject[] statistics;
+	public TextMeshProUGUI[] statistics;
 
 	void Start()
 	{
@@ -15,13 +16,13 @@ public class PrepareStatisticScene : MonoBehaviour {
 	{
 		int countTotalStars = PlayerPrefs.GetInt ("Category1Stars") + PlayerPrefs.GetInt ("Category2Stars") + PlayerPrefs.GetInt ("Category3Stars");
 
-		statistics [0].GetComponent<Text> ().text = PlayerPrefs.GetInt ("TotalNumberOfMoves").ToString ();
-		statistics [1].GetComponent<Text> ().text = PlayerPrefs.GetInt ("TotalNumberOfTurns").ToString ();
-		statistics [2].GetComponent<Text> ().text = PlayerPrefs.GetInt ("NumberOfStartedLevels").ToString ();
-		statistics [3].GetComponent<Text> ().text = PlayerPrefs.GetInt ("NumberOfCompletedLevels").ToString ();
-		statistics [4].GetComponent<Text> ().text = PlayerPrefs.GetInt ("PerfectedLevels").ToString ();
-		statistics [5].GetComponent<Text> ().text = countTotalStars.ToString ();
-		statistics [6].GetComponent<Text> ().text = PlayerPrefs.GetInt ("TotalNumberOfResets").ToString ();
-		statistics [7].GetComponent<Text> ().text = PlayerPrefs.GetInt ("NumberOfAppStarts").ToString ();
+		statistics [0].text = PlayerPrefs.GetInt ("TotalNumberOfMoves").ToString ();
+		statistics [1].text = PlayerPrefs.GetInt ("TotalNumberOfTurns").ToString ();
+		statistics [2].text = PlayerPrefs.GetInt ("NumberOfStartedLevels").ToString ();
+		statistics [3].text = PlayerPrefs.GetInt ("NumberOfCompletedLevels").ToString ();
+		statistics [4].text = PlayerPrefs.GetInt ("PerfectedLevels").ToString ();
+		statistics [5].text = countTotalStars.ToString ();
+		statistics [6].text = PlayerPrefs.GetInt ("TotalNumberOfResets").ToString ();
+		statistics [7].text = PlayerPrefs.GetInt ("NumberOfAppStarts").ToString ();
 	}
 }
